071d07ba6e0ed17ee6ac3c6f3bf8e22dfd07082c
samuelxu999 <samuelxu999@gmail.com>
2018-01-31 16:49:40 -0500

    Add smart contact demo SmartToken

---
+++ Projects/SmartToken/truffle.js
@@ -0,0 +1,12 @@
+module.exports = {
+	// See <http://truffleframework.com/docs/advanced/configuration>
+	// to customize your Truffle configuration!
+	networks: {
+			development: {
+			host: "localhost",
+			port: 8042,
+			network_id: "42", // Match any network id
+			gas: 4712388
+		}
+	}
+};
