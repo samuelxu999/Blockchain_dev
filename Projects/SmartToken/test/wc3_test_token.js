b0e6fce62f55f34644a201bd468d388aeda5125b
samuelxu999 <samuelxu999@gmail.com>
2018-02-24 17:51:44 -0500

    Update test/wc3_test_token.js

--- Projects/SmartToken/test/wc3_test_token.js
+++ Projects/SmartToken/test/wc3_test_token.js
@@ -3,14 +3,14 @@
 */
 
 // Interaction with Ethereum
-var Web3 = require('web3')
-var web3 = new Web3()
+var Web3 = require('web3');
+var web3 = new Web3();
 
 // connect to the local node
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8042'))
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8042'));
 
 // The contract that we are going to interact with
-var contractAddress = '0x1987802daa5798cfb5e881c109c4d448b6e4125b'
+var contractAddress = '0x1987802daa5798cfb5e881c109c4d448b6e4125b';
 
 // Load config data from SmartToken.json
 var config = require('../build/contracts/SmartToken.json');
@@ -22,7 +22,7 @@ var ABI = config.abi;
 //var ABI = JSON.parse('[ { "constant": false, "inputs": [ { "name": "recipient", "type": "address" }, { "name": "value", "type": "uint256" } ], "name": "depositToken", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "recipient", "type": "address" } ], "name": "getTokens", "outputs": [ { "name": "value", "type": "uint256", "value": "60" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "recipient", "type": "address" }, { "name": "value", "type": "uint256" } ], "name": "withdrawToken", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "_from", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" } ], "name": "OnValueChanged", "type": "event" } ]')
 
 // contract object
-var contract = web3.eth.contract(ABI).at(contractAddress)
+var contract = web3.eth.contract(ABI).at(contractAddress);
 
 //-------------------------- test demo -------------------------------
 var test_node = "node1_1";
@@ -52,6 +52,9 @@ function account_onValueChanged(address) {
 			//showStatus()
 			var token = contract.getTokens(address);
 			console.log(token);
+
+			//stop watching
+			onValueChanged.stopWatching();
 		}
 	})
 }
@@ -60,7 +63,7 @@ function account_onValueChanged(address) {
 function showStatus() {
  
 	// retrieve the value of the token
-	var token = contract.getTokens(web3.eth.coinbase)
+	var token = contract.getTokens(web3.eth.coinbase);
 
 	// display the value of the token in accounts[0]
 	console.log(token);
@@ -74,28 +77,32 @@ function list_all() {
 	
 	for (i = 0; i < web3.eth.accounts.length; i++) { 
 		// get token value 
-		var token = contract.getTokens(web3.eth.accounts[i])
+		var token = contract.getTokens(web3.eth.accounts[i]);
 		// display the value of the token according to account
 		console.log(token);
 	}
 }
 
 function get_token(node_name) {
-	var address=getAddress(node_name)
-	var token = contract.getTokens(address)
-	console.log(token);
+	var address=getAddress(node_name);
+	var token = contract.getTokens(address);
+
+	//print out token data
+	console.log(token.s);
+	console.log(token.e);
+	console.log(token["c"][0]);
 }
 
 // launch depositToken transaction
 function deposit_test(recipient, value) {
-	var ret=contract.depositToken(recipient, value, {from: web3.eth.coinbase})
+	var ret=contract.depositToken(recipient, value, {from: web3.eth.coinbase});
 	// display the tracsaction result
 	console.log(ret);
 }
 
 // launch withdrawToken transaction
 function withdraw_test(recipient, value) {
-	var ret=contract.withdrawToken(recipient, value, {from: web3.eth.coinbase})
+	var ret=contract.withdrawToken(recipient, value, {from: web3.eth.coinbase});
 	// display the tracsaction result
 	console.log(ret);
 }
