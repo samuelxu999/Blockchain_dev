efed61bca616296fd7de1b0a3f85ab81cfd752de
samuelxu999 <samuelxu999@gmail.com>
2018-03-08 16:50:51 -0500

    Add python API to access CapAC token contract

---
+++ Projects/py_dev/CapAC_Token.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3.5
+
+'''
+========================
+Capability token module
+========================
+Created on Mar.8, 2018
+@author: Xu Ronghua
+@Email:  rxu22@binghamton.edu
+@TaskDescription: This module provide encapsulation of web3.py API to interact with smart contract.
+'''
+
+from web3 import Web3, HTTPProvider, IPCProvider
+from utilities import DatetimeUtil
+import json, datetime, time
+
+class CapACToken(object):
+	def __init__(self, http_provider, contract_addr, contract_config):
+		# configuration initialization
+		self.web3 = Web3(HTTPProvider(http_provider))
+		self.contract_address=Web3.toChecksumAddress(contract_addr)
+		self.contract_config = json.load(open(contract_config))
+
+		# new contract object
+		self.contract=self.web3.eth.contract()
+		self.contract.address=self.contract_address
+		self.contract.abi=self.contract_config['abi']
+
+	def Show_ContractInfo(self):  
+		print(self.web3.eth.blockNumber)
+		print(self.web3.eth.accounts)
+		print(self.web3.fromWei(self.web3.eth.getBalance(self.web3.eth.accounts[0]), 'ether'))
+
+		print(self.contract.address)
+
+	# return accounts address
+	def getAccounts(self):
+		return self.web3.eth.accounts
+
+	# return accounts balance
+	def getBalance(self, account_addr=''):
+		if(account_addr==''):
+			# get accounts[0] balance
+			checksumAddr=self.web3.eth.coinbase
+		else:
+			#Change account address to EIP checksum format
+			checksumAddr=Web3.toChecksumAddress(account_addr)	
+		return self.web3.fromWei(self.web3.eth.getBalance(checksumAddr), 'ether')
+
+	#get token data by call getCapToken()
+	def getCapToken(self, account_addr):
+		#Change account address to EIP checksum format
+		checksumAddr=Web3.toChecksumAddress(account_addr)
+
+		'''
+		Call a contract function, executing the transaction locally using the eth_call API. 
+		This will not create a new public transaction.
+		'''
+		token_data=self.contract.call({'from': self.web3.eth.coinbase}).getCapToken(checksumAddr)
+		return token_data
+
+	#initialized token by sending transact to call initCapToken()
+	def initCapToken(self, account_addr):
+		#Change account address to EIP checksum format
+		checksumAddr=Web3.toChecksumAddress(account_addr)
+
+		# Execute the specified function by sending a new public transaction.
+		ret=self.contract.transact({'from': self.web3.eth.coinbase}).initCapToken(checksumAddr)
+
+	# set isValid flag in token
+	def setCapToken_isValid(self, recipient, flag_value):
+		#Change account address to EIP checksum format
+		checksumAddr=Web3.toChecksumAddress(recipient)	
+
+		# Execute the specified function by sending a new public transaction.	
+		ret=self.contract.transact({'from': self.web3.eth.coinbase}).setCapToken_isValid(checksumAddr, flag_value);
+
+	# set issue date and expired date
+	def setCapToken_expireddate(self, recipient, issue_time, expire_time):
+		#Change account address to EIP checksum format
+		checksumAddr=Web3.toChecksumAddress(recipient)	
+
+		# Execute the specified function by sending a new public transaction.	
+		ret=self.contract.transact({'from': self.web3.eth.coinbase}).setCapToken_expireddate(checksumAddr, issue_time, expire_time)
+
+	# set issue date and expired date
+	def setCapToken_authorization(self, recipient, access_right):
+		#Change account address to EIP checksum format
+		checksumAddr=Web3.toChecksumAddress(recipient)	
+
+		# Execute the specified function by sending a new public transaction.	
+		ret=self.contract.transact({'from': self.web3.eth.coinbase}).setCapToken_authorization(checksumAddr, access_right);
+
+	# Print token date
+	@staticmethod
+	def print_tokendata(token_data):
+		#print(token_data)
+		for i in range(0,len(token_data)):
+			if(i==4 or i==5):
+				dt=DatetimeUtil.timestamp_datetime(token_data[i])
+				#dt=datetime.datetime.utcfromtimestamp(token_data[i]/1e3)
+				print(DatetimeUtil.datetime_string(dt))
+				#print(DatetimeUtil.datetime_timestamp(dt))
+				#print(token_data[i])
+			else:
+				print(token_data[i])
+	
+	# get address from json file
+	@staticmethod
+	def getAddress(node_name, datafile):
+		address_json = json.load(open(datafile))
+		return address_json[node_name]
+
+if __name__ == "__main__":
+	http_provider = 'http://localhost:8042'
+	contract_addr = '0x9ccaa35b84570597fce4db8b157ae5cebfb4d0e5'
+	contract_config = '../CapbilityToken/build/contracts/CapACToken.json'
+
+	#Get account address
+	accountAddr=CapACToken.getAddress('sam_miner_win7_0', '../CapbilityToken/test/addr_list.json')
+	print("Account: " + accountAddr)
+	#new CapACToken object
+	mytoken=CapACToken(http_provider, contract_addr, contract_config)
+	#mytoken.Show_ContractInfo()
+
+
+	#------------------------- test contract API ---------------------------------
+	#getAccounts
+	accounts = mytoken.getAccounts()
+	balance = mytoken.getBalance('0x950d8eb4825c597534027638c862496ea0d7cf43')
+	print(accounts)
+	print(balance)
+
+	#Read token data using call
+	token_data=mytoken.getCapToken(accountAddr);
+	CapACToken.print_tokendata(token_data)
+
+	#Send transact
+	#mytoken.initCapToken(accountAddr);
+	#mytoken.setCapToken_isValid(accountAddr, True)
+
+	#set issue date and expired date
+	nowtime = datetime.datetime.now()
+	#calculate issue_time and expire_time
+	issue_time = DatetimeUtil.datetime_timestamp(nowtime)
+	duration = DatetimeUtil.datetime_duration(0, 1, 0, 0)
+	expire_time = DatetimeUtil.datetime_timestamp(nowtime+duration)
+	#mytoken.setCapToken_expireddate(accountAddr, issue_time, expire_time)
+
+	access_right='{"resource":"/test/api/v1.0/dt, "action":"GET"}';
+	#mytoken.setCapToken_authorization(accountAddr, access_right)
+
+	pass
---
+++ Projects/py_dev/utilities.py
@@ -0,0 +1,207 @@
+'''
+========================
+utilities.py
+========================
+Created on Oct.23, 2017
+@author: Xu Ronghua
+@Email:  rxu22@binghamton.edu
+@TaskDescription: This module provide utility function to support project.
+@Reference: 
+'''
+
+from datetime import datetime, timedelta
+import json
+
+'''
+FileUtil class for handling file data
+'''
+class FileUtil(object):
+
+	'''
+	Function:read line contents from file
+	@arguments: 
+	(in)  filepath:   	input file path
+	(out) ls_lines:   	return line list object
+	'''
+	@staticmethod
+	def ReadLines(filepath):
+		#define file handle to open select file
+		fname = open(filepath, 'r')    
+		#read text by line and saved as array list ls_lines
+		ls_lines=fname.readlines()
+		#close file
+		fname.close()
+		return ls_lines
+	
+	'''
+	Function: write line string to file
+	@arguments: 
+	(in)  filepath:   	input file path
+	(in)  linedata:   	line data for writing
+	'''
+	@staticmethod
+	def AddLine(filepath, linedata):
+		#define file handle to open select file for appending data
+		fname = open(filepath, 'a') 
+		
+		#write line data to file
+		fname.write("%s\n" %(linedata))
+		
+		#close file
+		fname.close()
+	
+	'''
+	Function: write list data to file by each line
+	@arguments: 
+	(in)  filepath:   	input file path
+	(in)  ls_data:   	list data for writing
+	'''
+	@staticmethod
+	def AddDataByLine(filepath, ls_data):
+		#define file handle to open select file for write data
+		fname = open(filepath, 'w') 
+		
+		#for each lines in data and write to file
+		for linedata in ls_data:
+			#write line data to file
+			fname.write("%s\n" %(linedata))
+		
+		#close file
+		fname.close()
+	
+	'''
+	Function: remove line containing target string from file
+	@arguments: 
+	(in)  filepath:   	input file path
+	(in)  str_target:   target string for delete
+	'''
+	@staticmethod
+	def DeleteLine(filepath, str_target):
+		#First, read all data from file
+		fname = open(filepath, 'r')   
+		ls_lines=fname.readlines()	
+		fname.close()	
+
+		#reopen file with 'w' option
+		fname = open(filepath, 'w')
+		
+		#for each line to rewrite all data except ls_data
+		for line in ls_lines:
+			if((str_target in line) or line=='\n'):
+				continue
+			#write line data to file
+			fname.write("%s" %(line))
+		#close file
+		fname.close()
+
+	'''
+	Function: update line containing target string in file
+	@arguments: 
+	(in)  filepath:   	input file path
+	(in)  str_target:   target string for delete
+	'''		
+	@staticmethod
+	def UpdateLine(filepath, str_target, str_line):
+		#First, read all data from file
+		fname = open(filepath, 'r')   
+		ls_lines=fname.readlines()	
+		fname.close()	
+
+		#reopen file with 'w' option
+		fname = open(filepath, 'w')
+		
+		#for each line to rewrite all data
+		for line in ls_lines:
+			if(str_target in line):
+				#write updated data to file
+				fname.write("%s" %(str_line))
+			else:
+				#write unchanged line to file
+				fname.write("%s" %(line))
+		#close file
+		fname.close()
+		
+'''
+DatetimeUtil class for format handle between datatime and string
+'''
+class DatetimeUtil(object):
+	#switch int timestamp to datatime object
+	@staticmethod
+	def timestamp_datetime(_timestamp):
+		obj_datetime=datetime.fromtimestamp(_timestamp/1e3)
+		return obj_datetime
+
+	#switch datatime object to int timestamp
+	@staticmethod
+	def datetime_timestamp(_datetime):
+		int_datetime=int(_datetime.timestamp() * 1e3)
+		return int_datetime
+
+	#switch datatime object to format string
+	@staticmethod
+	def datetime_string(_datetime, _format="%Y-%m-%d %H:%M:%S"):
+		str_datetime=_datetime.strftime(_format)
+		return str_datetime
+		
+	#switch format string to datatime object
+	@staticmethod
+	def string_datetime(_strtime, _format="%Y-%m-%d %H:%M:%S"):
+		_datetime=datetime.strptime(_strtime, _format)
+		return _datetime
+		
+	#get datatime duration from format input
+	@staticmethod
+	def datetime_duration(_weeks=0,_days=0,_hours=0,_minutes=0):
+		_duration=timedelta(weeks=_weeks,days=_days,hours=_hours,minutes=_minutes)
+		return _duration
+		
+	#check whether input datatime has expired
+	@staticmethod
+	def IsExpired(str_datatime):
+		time1=DatetimeUtil.string_datetime(str_datatime)
+		time2=datetime.now()
+		diff = (time1-time2).total_seconds()
+		if(diff>0):
+			return False
+		else:
+			return True
+			
+'''
+TypesUtil class for data type format transfer
+'''
+class TypesUtil(object):
+	#string to hex
+	@staticmethod
+	def string_to_hex(str_data):
+		hex_data=str_data.hex()
+		return hex_data
+		
+	#hex to string
+	@staticmethod
+	def hex_to_string(hex_data):
+		str_data=bytes.fromhex(hex_data)
+		return str_data
+		
+	#string to bytes
+	@staticmethod
+	def string_to_bytes(str_data):
+		bytes_data=str_data.encode(encoding='UTF-8')
+		return bytes_data
+		
+	#bytes to string
+	@staticmethod
+	def bytes_to_string(byte_data):
+		str_data=byte_data.decode(encoding='UTF-8')
+		return str_data
+		
+	#string to json
+	@staticmethod
+	def string_to_json(json_str):
+		json_data = json.loads(json_str)
+		return json_data
+		
+	#json to string
+	@staticmethod
+	def json_to_string(json_data):
+		json_str = json.dumps(json_data)
+		return json_str
