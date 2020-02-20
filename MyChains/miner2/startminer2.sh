2f2a9ad7631fbf40a0fce37336fbe04f77daa8fe
samuelxu999 <rxu22@binghamton.edu>
2020-02-20 12:21:21 -0500

    Update env_setup.txt and Mychain setup.

--- MyChains/miner2/startminer2.sh
+++ MyChains/miner2/startminer2.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-geth --identity "miner2" --networkid 42 --datadir "~/Desktop/Github/Blockchain_dev/MyChains/miner2" --nodiscover --mine --rpc --rpcport "8043" --port "30304" --unlock 0 --password ~/Desktop/Github/Blockchain_dev/MyChains/miner2/password.sec
+geth --identity "miner2" \
+--networkid 42 \
+--datadir "~/Desktop/Github/Blockchain_dev/MyChains/miner2" \
+--nodiscover \
+--mine \
+--minerthreads "1" \
+--rpc --rpcport "8043" \
+--port "30304" \
+--allow-insecure-unlock \
+--unlock 0 \
+--password ~/Desktop/Github/Blockchain_dev/MyChains/miner2/password.sec
