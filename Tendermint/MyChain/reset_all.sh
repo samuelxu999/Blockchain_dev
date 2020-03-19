4f7285c2adaeddd2ec003b98331b401cb794a7ed
samuelxu999 <rxu22@binghamton.edu>
2020-03-19 10:51:20 -0400

    update reset.all.sh

--- Tendermint/MyChain/reset_all.sh
+++ Tendermint/MyChain/reset_all.sh
@@ -8,3 +8,6 @@ tendermint unsafe_reset_all --home $NODE_DIR
 
 # (unsafe) Reset this node's validator to genesis state
 # tendermint  unsafe_reset_priv_validator --home $NODE_DIR
+
+# copy genesis.json to node/config/ to replace with old one
+cp genesis.json node/config/
